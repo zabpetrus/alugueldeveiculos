c80ff7aa2a5f9176b0dd9506e56666705f21a6d8
belsanpesou <=>
2022-04-05 00:19:15 -0300

    Criando as páginas e blocos

--- frontend/src/app/components/agendamento/agendamento.component.ts
+++ frontend/src/app/components/agendamento/agendamento.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import '../../pages/carros/carros.component.css';
 
 @Component({
   selector: 'app-agendamento',
@@ -7,7 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgendamentoComponent implements OnInit {
 
-  constructor() { }
+
+
+  constructor(private router:Router) { }
+
+  nextStep(){
+    const page2: string[] = ['carros'];
+    this.router.navigate(page2);
+  };
 
   ngOnInit(): void {
   }
